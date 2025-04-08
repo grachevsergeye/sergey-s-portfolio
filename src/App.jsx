9b5c02a3e19d146b91c59bfa4d721f88b83a8cfe
grachevsergeye <sergey_grachev_03@mail.ru>
2025-04-09 02:00:18 +0300

    Removing a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import LazyLoad from "react-lazyload";
-import { lazy, Suspense, useEffect } from "react";
+import { useEffect } from "react";
 import Hero from "./components/hero/Hero";
 import Contact from "./components/contact/Contact";
 import Services from "./components/services/Services";
-
-const Portfolio = lazy(() => import("./components/portfolio/Portfolio"));
+import Portfolio from "./components/portfolio/Portfolio";
 
 const App = () => {
 
@@ -22,20 +20,16 @@ const App = () => {
 
   return (
     <div className="container">
-           {/* Disabled lazy loading for button and scroll to work properly */}
+
           <section id="home">
             <Hero />
           </section>
 
           <section id="services">
             <Services />
-          </section>{" "}
+          </section>
 
-      <Suspense fallback={"loading..."}>
-        <LazyLoad height={"600vh"} offset={-100}>
-          <Portfolio />
-        </LazyLoad>
-      </Suspense>
+            <Portfolio />
 
       <section id="contact">
         <Contact />
